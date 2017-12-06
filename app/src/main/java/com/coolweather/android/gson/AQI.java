99ffc801405e159ecb23e1846989468afdbc3956
miss <1216789028@qq.com>
2017-12-07 01:30:10 +0800

    Second commit

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by miss on 2017/12/5.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
